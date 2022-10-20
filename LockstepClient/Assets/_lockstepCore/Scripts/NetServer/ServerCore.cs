e831b10763d8f9831c9b38c8d1053ebfba23dafa
delevin.ying <846353427@qq.com>
2022-10-20 19:31:33 +0800

    整理代码

--- LockstepClient/Assets/_lockstepCore/Scripts/NetServer/ServerCore.cs
+++ LockstepClient/Assets/_lockstepCore/Scripts/NetServer/ServerCore.cs
@@ -11,33 +11,35 @@ using System.Diagnostics;
 
 public class ServerCore : MonoBehaviour
 {
+    [Header("房间玩家数量")]
+    public int RoomPlayerNumber = 1;
+    [Header("端口号")]
+    public int Port = 9050;
+
+    private string _ip;
+
+    [HideInInspector]
+    public string SetGet_str_ipAddress
+    {
+        set { _ip = value; }
+        get
+        {
+            _ip = NetTool.IP(address.IPv4);
+            return _ip;
+        }
+    }
+
     LiteNetLibServer server;
     // Start is called before the first frame update
     void Start()
     {
-        int roomSize = 2;
-        //int waitInSeconds = 5;
-
-        //var sw = new Stopwatch();
-        //sw.Start();
-        //Console.Write($"Enter room size (defaults to {roomSize} after {waitInSeconds} seconds): ");
-        //while (!Console.KeyAvailable && sw.Elapsed.Seconds < waitInSeconds)
-        //{
-        //    Thread.Sleep(1);
-        //}
-        //sw.Stop();
-
-        //if (Console.KeyAvailable)
-        //{
-        //    roomSize = Console.ReadKey().KeyChar - 48;
-        //}
-        //Console.Write(Environment.NewLine);
+        //int roomSize = 2;
 
         server = new LiteNetLibServer();
 
-        var room = new Lockstep.Network.Server.Room(server, roomSize);
+        var room = new Lockstep.Network.Server.Room(server, RoomPlayerNumber);
 
-        room.Open(9050);
+        room.Open(Port);
 
 
     }
@@ -46,5 +48,15 @@ public class ServerCore : MonoBehaviour
     void Update()
     {
         server.PollEvents();
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 300, 100), $"ip:{SetGet_str_ipAddress} ");
+        GUI.Label(new Rect(0, 30, 300, 100), $"port:{Port} ");
+        GUI.Label(new Rect(0, 60, 300, 100), $"RoomPlayer:{RoomPlayerNumber} ");
+
+
+        GUI.Label(new Rect(0, 90, 300, 100), $"ReadyPlayer:{RoomPlayerNumber} ");
     }
 }
