9813e83f818eb1a50e8ccc0817828507913c1d2b
delevin.ying <846353427@qq.com>
2022-10-20 09:56:25 +0800

    link

--- Link.bat
+++ Link.bat
@@ -3,15 +3,15 @@ cd /d %~dp0
 cd "LockstepServer"
 
 rd /s /Q Assets
-mklink /J Assets "%~dp0/Core/Assets"
+mklink /J Assets "%~dp0/LockstepClient/Assets"
 
 
 rd /s /Q Packages
-mklink /J Packages "%~dp0/Core/Packages"
+mklink /J Packages "%~dp0/LockstepClient/Packages"
 
 
 rd /s /Q ProjectSettings
-mklink /J ProjectSettings "%~dp0/Core/ProjectSettings"
+mklink /J ProjectSettings "%~dp0/LockstepClient/ProjectSettings"
 
 echo "Successed..."
 
