1ba3c7985c90867d35c1b2b82515780085ba6805
delevin.ying <846353427@qq.com>
2022-10-28 18:38:31 +0800

    输入系统

---
+++ LockstepClient/Assets/Script/Input/InputHandler.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour, PlayerInput.IPlayerActions, PlayerInput.IUIActions
+{
+    private PlayerInput playerInput;
+
+    public void OnCancel(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnMiddleClick(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log(context.ToString());
+    }
+
+    public void OnNavigate(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnRightClick(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnScrollWheel(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnSubmit(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnTrackedDeviceOrientation(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnTrackedDevicePosition(InputAction.CallbackContext context)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        playerInput = new PlayerInput();
+        playerInput.Player.SetCallbacks(this);
+        //playerInput.UI.SetCallbacks(this);
+        //playerInput.Enable();
+    }
+
+
+
+    // Start is called before the first frame update
+
+
+
+
+
+}
