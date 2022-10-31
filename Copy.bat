e7b6841bf1a0b2488a43448dc76a54c49b5dcfa1
delevin.ying <846353427@qq.com>
2022-10-31 17:44:45 +0800

    copy client

---
+++ Copy.bat
@@ -0,0 +1,26 @@
+cd /d %~dp0
+echo "Start..."
+
+cd "LockstepCopyClient"
+
+
+
+
+
+rd /s /Q Assets
+mklink /J Assets "%~dp0/LockstepClient/Assets"
+
+	
+rd /s /Q Packages
+mklink /J Packages "%~dp0/LockstepClient/Packages"
+
+
+
+rd /s /Q ProjectSettings
+mklink /J ProjectSettings "%~dp0/LockstepClient/ProjectSettings"
+
+
+
+echo "Successed..."
+
+pause
