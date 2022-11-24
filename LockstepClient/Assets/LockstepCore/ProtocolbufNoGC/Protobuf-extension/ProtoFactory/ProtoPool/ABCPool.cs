823eff24aebe371ef0a1f056f1b2731b4bb5ea68
delevin.ying <846353427@qq.com>
2022-11-24 10:34:32 +0800

    dev_protocolbuffer 手动合入 ,合入部分文件

---
+++ LockstepClient/Assets/LockstepCore/ProtocolbufNoGC/Protobuf-extension/ProtoFactory/ProtoPool/ABCPool.cs
@@ -0,0 +1,16 @@
+﻿using Protocol;
+
+public sealed class ABCPool : ProtoPoolBase<ABC>
+{
+    protected override void ClearNetData(ABC netData)
+    {
+        netData.IVal = 0;
+        netData.FVal = 0f;
+        netData.UVal = 0;
+    }
+
+    protected override void RecycleChildren(ABC data)
+    {
+        // TODO:不含引用就不需要写
+    }
+}
