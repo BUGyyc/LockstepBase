9f57bd7c122bb0141bcddcce8fc89cbfdfe35233
delevin.ying <846353427@qq.com>
2022-11-24 10:29:59 +0800

    dev_protocolbuffer 手动合入 ,合入部分文件

---
+++ LockstepClient/Assets/LockstepCore/Record/RecordManager.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * @Author: delevin.ying 
+ * @Date: 2022-11-18 18:21:37 
+ * @Last Modified by: delevin.ying
+ * @Last Modified time: 2022-11-18 18:24:29
+ */
+
+using System.Collections.Generic;
+using Lockstep.Core.Logic.Interfaces;
+using UnityEngine;
+public class RecordManager
+{
+    public static RecordManager Instance;
+
+    public Queue<ICommand> cacheQueue;
+
+    public void AddInputCommand(ICommand command)
+    {
+        Debug.Log($"保存指令 --- {command}");
+        cacheQueue.Enqueue(command);
+    }
+
+
+    public void Save2File()
+    {
+
+    }
+
+    public void LoadInputCommandByFile()
+    {
+
+    }
+}
