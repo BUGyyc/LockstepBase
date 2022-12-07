53d41921b10611e41fe5bdbd283e755a245647df
delevin.ying <846353427@qq.com>
2022-12-07 16:06:51 +0800

    整理框架

--- LockstepClient/Assets/Script/GameSceneSetting.cs
+++ LockstepClient/Assets/Script/GameSceneSetting.cs
@@ -1,8 +1,8 @@
 ﻿public static class GameSceneSetting
 {
-    public const string RTS_SCENE = "RTS";
-    public const string TPS_SCENE = "TPS";
-    public const string ACTION_SCENE = "Action";
+    // public const string RTS_SCENE = "RTS";
+    // public const string TPS_SCENE = "TPS";
+    public const string ACTION_SCENE = "Battle";
 
     public const string LaunchClient = "LaunchClient";
     public const string LaunchClientPath = "Assets/Scenes/Debug/0.launch/LaunchClient.unity";
