8a4d6477e1d5eafbc5a8e0b57220d6a0440037c0
delevin.ying <846353427@qq.com>
2023-04-15 18:58:44 +0800

    timeline

--- LockstepClient/Assets/LockstepCore/Common/Task/Core/TaskCondition.cs
+++ LockstepClient/Assets/LockstepCore/Common/Task/Core/TaskCondition.cs
@@ -1,1 +1,46 @@
-﻿
+﻿/*
+ * @Author: delevin.ying 
+ * @Date: 2023-04-15 18:52:17 
+ * @Last Modified by: delevin.ying
+ * @Last Modified time: 2023-04-15 18:57:31
+ */
+
+using System.Collections.Generic;
+
+namespace TaskCore
+{
+
+    public class TaskCondition
+    {
+        public Queue<TaskConditionItem> conditionQueue;
+    }
+
+
+    public class TaskConditionItem
+    {
+        public TaskConditionMode mode;
+
+        public System.Action callAction;
+
+        public bool CheckItemResult()
+        {
+            return false;
+        }
+    }
+
+
+
+
+
+    public enum TaskConditionMode
+    {
+        Default = 0,
+
+        And = 1,
+        Or = 2,
+
+        Not = 3
+    }
+
+
+}
