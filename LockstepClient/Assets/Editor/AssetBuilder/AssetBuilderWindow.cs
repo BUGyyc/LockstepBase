51212df6e4ca069d2bef387983f610fbb1e0af70
delevin.ying <846353427@qq.com>
2023-04-23 19:47:54 +0800

    AB

--- LockstepClient/Assets/Editor/AssetBuilder/AssetBuilderWindow.cs
+++ LockstepClient/Assets/Editor/AssetBuilder/AssetBuilderWindow.cs
@@ -2,7 +2,7 @@
  * @Author: delevin.ying 
  * @Date: 2023-04-23 17:47:41 
  * @Last Modified by: delevin.ying
- * @Last Modified time: 2023-04-23 19:30:10
+ * @Last Modified time: 2023-04-23 19:47:13
  */
 
 using UnityEditor;
@@ -13,6 +13,7 @@ namespace AssetBuilderCore
 {
     public class AssetBuilderWindow : EditorWindow
     {
+
 
         [MenuItem("项目工具/打包")]
         private static void ShowWindow()
@@ -28,8 +29,133 @@ namespace AssetBuilderCore
             {
                 AutoABLabel();
             }
+
+
+            if (GUILayout.Button("CreateAB"))
+            {
+                CreateAB();
+            }
         }
 
+
+        const string AssetBundlesOutputPath = "Assets/OutPut";
+
+        public void CreateAB()
+        {
+            ClearAssetBundlesName();
+            string sourcePath = Application.dataPath + "/Resources";
+            Pack(sourcePath);
+
+            string outputPath = Path.Combine(AssetBundlesOutputPath, Platform.GetPlatformFolder(EditorUserBuildSettings.activeBuildTarget));
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+
+            //根据BuildSetting里面所激活的平台进行打包 设置过AssetBundleName的都会进行打包
+            BuildPipeline.BuildAssetBundles(outputPath, 0, EditorUserBuildSettings.activeBuildTarget);
+
+            AssetDatabase.Refresh();
+
+            Debug.Log("打包完成");
+        }
+
+
+        /// <summary>
+        /// 清除之前设置过的AssetBundleName，避免产生不必要的资源也打包
+        /// 之前说过，只要设置了AssetBundleName的，都会进行打包，不论在什么目录下
+        /// </summary>
+        static void ClearAssetBundlesName()
+        {
+            int length = AssetDatabase.GetAllAssetBundleNames().Length;
+            Debug.Log(length);
+            string[] oldAssetBundleNames = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                oldAssetBundleNames[i] = AssetDatabase.GetAllAssetBundleNames()[i];
+            }
+
+            for (int j = 0; j < oldAssetBundleNames.Length; j++)
+            {
+                AssetDatabase.RemoveAssetBundleName(oldAssetBundleNames[j], true);
+            }
+            length = AssetDatabase.GetAllAssetBundleNames().Length;
+            Debug.Log(length);
+        }
+
+        static void Pack(string source)
+        {
+            //Debug.Log("Pack source " + source);
+            DirectoryInfo folder = new DirectoryInfo(source);
+            FileSystemInfo[] files = folder.GetFileSystemInfos();
+            int length = files.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (files[i] is DirectoryInfo)
+                {
+                    Pack(files[i].FullName);
+                }
+                else
+                {
+                    if (!files[i].Name.EndsWith(".meta"))
+                    {
+                        fileWithDepends(files[i].FullName);
+                    }
+                }
+            }
+        }
+        //设置要打包的文件
+        static void fileWithDepends(string source)
+        {
+            Debug.Log("file source " + source);
+            string _source = Replace(source);
+            string _assetPath = "Assets" + _source.Substring(Application.dataPath.Length);
+
+            Debug.Log(_assetPath);
+
+            //自动获取依赖项并给其资源设置AssetBundleName
+            string[] dps = AssetDatabase.GetDependencies(_assetPath);
+            foreach (var dp in dps)
+            {
+                Debug.Log("dp " + dp);
+                if (dp.EndsWith(".cs"))
+                    continue;
+                AssetImporter assetImporter = AssetImporter.GetAtPath(dp);
+                string pathTmp = dp.Substring("Assets".Length + 1);
+                string assetName = pathTmp.Substring(pathTmp.IndexOf("/") + 1);
+                assetName = assetName.Replace(Path.GetExtension(assetName), ".data");
+                Debug.Log(assetName);
+                assetImporter.assetBundleName = assetName;
+            }
+
+        }
+
+        //设置要打包的文件
+        static void file(string source)
+        {
+            Debug.Log("file source " + source);
+            string _source = Replace(source);
+            string _assetPath = "Assets" + _source.Substring(Application.dataPath.Length);
+            string _assetPath2 = _source.Substring(Application.dataPath.Length + 1);
+            //Debug.Log (_assetPath);
+
+            //在代码中给资源设置AssetBundleName
+            AssetImporter assetImporter = AssetImporter.GetAtPath(_assetPath);
+            string[] dps = AssetDatabase.GetDependencies(_assetPath);
+            foreach (var dp in dps)
+            {
+                Debug.Log("dp " + dp);
+            }
+            string assetName = _assetPath2.Substring(_assetPath2.IndexOf("/") + 1);
+            assetName = assetName.Replace(Path.GetExtension(assetName), ".unity3d");
+            Debug.Log(assetName);
+            assetImporter.assetBundleName = assetName;
+        }
+
+        static string Replace(string s)
+        {
+            return s.Replace("\\", "/");
+        }
 
 
         public void AutoABLabel()
---
+++ LockstepClient/Assets/Editor/AssetBuilder/Platform.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * @Author: delevin.ying 
+ * @Date: 2023-04-23 19:42:04 
+ * @Last Modified by: delevin.ying
+ * @Last Modified time: 2023-04-23 19:42:40
+ */
+using UnityEngine;
+using UnityEditor;
+
+
+public class Platform
+{
+    public static string GetPlatformFolder(BuildTarget target)
+    {
+        switch (target)
+        {
+            case BuildTarget.Android:
+                return "Android";
+            case BuildTarget.iOS:
+                return "IOS";
+            // case BuildTarget.WebPlayer:
+            //     return "WebPlayer";
+            case BuildTarget.StandaloneWindows:
+            case BuildTarget.StandaloneWindows64:
+                return "Windows";
+            case BuildTarget.StandaloneOSXIntel:
+            case BuildTarget.StandaloneOSXIntel64:
+                // case BuildTarget.StandaloneOSXUniversal:
+                return "OSX";
+            default:
+                return null;
+        }
+    }
+}
