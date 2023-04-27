68f00de6997e53f3dd7bf6d039e391b75ebea7ae
delevin.ying <846353427@qq.com>
2023-04-27 17:11:12 +0800

    精简首包构建

---
+++ LockstepClient/Assets/AssetBuilder/DataStruct/WebLoadProgress.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Networking;
+
+
+public class WebLoadProgress
+{
+    internal UnityWebRequestAsyncOperation WeqOperation;
+
+    internal float GetWebProgress()
+    {
+        if (WeqOperation != null)
+        {
+            return WeqOperation.progress;
+        }
+        return 1;
+    }
+}
