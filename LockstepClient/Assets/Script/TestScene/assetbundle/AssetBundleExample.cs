0c921cdfd3d1f246ebc997fb5c8eeeefa6ef0187
delevin.ying <846353427@qq.com>
2023-04-28 14:33:21 +0800

    log 工具

--- LockstepClient/Assets/Script/TestScene/assetbundle/AssetBundleExample.cs
+++ LockstepClient/Assets/Script/TestScene/assetbundle/AssetBundleExample.cs
@@ -2,52 +2,58 @@
 using System;
 using System.IO;
 // using ABCore;
-namespace ET.Client
+//namespace ET.Client
+//{
+public class AssetBundleExample : MonoBehaviour
 {
-    public class AssetBundleExample : MonoBehaviour
+    void Start()
     {
-        void Start()
-        {
-            // AssetBundleHelper.instance.L
+        // AssetBundleHelper.instance.L
 
 
-            //AssetBundleHelper.instance.LoadFile("Bullet", AssetFileType.Prefab, (obj) =>
-            //{
-            //    GameObject ins = GameObject.Instantiate(obj);
-            //    obj.transform.position = Vector3.zero;
-            //});
+        LogMaster.L("Hello Start");
 
-
-
-            // Singleton<UpdateABPipeline>.instance.Test();
-
-
-            //Func().Coroutine();
-        }
-
-
-        // private async ETVoid Func()
-        // {
-        //     //Debug.Log("3");
-
-        //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("hero");
-
-        //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("bullet");
-
-
-        //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("canvas");
-
-
-        //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("enemy");
-
-        //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("clazyrunnner");
-
-
-        //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("player");
-        // }
+        //AssetBundleHelper.instance.LoadFile("Bullet", AssetFileType.Prefab, (obj) =>
+        //{
+        //    GameObject ins = GameObject.Instantiate(obj);
+        //    obj.transform.position = Vector3.zero;
+        //});
 
 
 
+        // Singleton<UpdateABPipeline>.instance.Test();
 
+
+        //Func().Coroutine();
     }
+
+    private void Update()
+    {
+        LogMaster.E("AB  Update");
+    }
+
+    // private async ETVoid Func()
+    // {
+    //     //Debug.Log("3");
+
+    //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("hero");
+
+    //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("bullet");
+
+
+    //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("canvas");
+
+
+    //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("enemy");
+
+    //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("clazyrunnner");
+
+
+    //     await AssetBundleHelper.instance.AsyncLoadAssetBundle<AssetBundle>("player");
+    // }
+
+
+
+
 }
+//}
